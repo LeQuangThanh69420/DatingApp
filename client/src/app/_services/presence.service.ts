2b06fe7501c266b420fc7b423ae5184d4dea1a4d
then <lequanhlol@gmail.com>
2024-07-23 03:19:59 +0700

    end chapter 17.6 - Displaying online presence

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -3,6 +3,7 @@ import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,8 @@ import { ToastrService } from 'ngx-toastr';
 export class PresenceService {
   hubUrl = environment.hubUrl;
   private hubConnection: HubConnection;
+  private onlineUsersSource = new BehaviorSubject<string[]>([]);
+  onlineUsers$ = this.onlineUsersSource.asObservable();
 
   constructor(private toastr: ToastrService) { }
 
@@ -31,6 +34,10 @@ export class PresenceService {
 
     this.hubConnection.on("UserIsOffline", username => {
       this.toastr.warning(username + " has disconnected");
+    });
+
+    this.hubConnection.on("GetOnlineUsers", (usernames: string[]) => {
+      this.onlineUsersSource.next(usernames);
     });
   }
 
