96bb7b34a50af7b1c87ac89a11fd6e8f741af42e
then <lequanhlol@gmail.com>
2024-07-28 10:01:45 +0700

    end chapter 17.15 - Notifying users when they receive a message

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -3,7 +3,8 @@ import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 import { ToastrService } from 'ngx-toastr';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, take } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class PresenceService {
   private onlineUsersSource = new BehaviorSubject<string[]>([]);
   onlineUsers$ = this.onlineUsersSource.asObservable();
 
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService, private router: Router) { }
 
   createHubConnection(user: User) {
     this.hubConnection = new HubConnectionBuilder()
@@ -38,6 +39,13 @@ export class PresenceService {
 
     this.hubConnection.on("GetOnlineUsers", (usernames: string[]) => {
       this.onlineUsersSource.next(usernames);
+    });
+
+    this.hubConnection.on("NewMessageReceived", ({username, knownAs}) => {
+      this.toastr.info(knownAs + " has sent you a message!")
+        .onTap.pipe(take(1)).subscribe(() => {
+          this.router.navigateByUrl("/members/" + username + "?tab=3");
+        });
     });
   }
 
